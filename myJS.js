d2fb02924c7001afd28c052069d895510152bdaa
pa8rut <pa8rut@gmail.com>
2016-12-28 11:36:38 -0500

    update

--- myJS.js
+++ myJS.js
@@ -1,1 +1,11 @@
-
+$("#myButton").click(function(){
+  var myInput = $("#myInput");
+  
+  var myURL = "https://pokeapi.co/api/v2/pokemon/" + myInput + "/";
+  
+  $.ajax({
+    method: "GET",
+    url: "myURL",
+    success: function(data);
+  });
+});
